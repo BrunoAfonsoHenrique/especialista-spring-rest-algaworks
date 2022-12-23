5db3e43ced36d963e51eeafb137e9d7485ff50f3
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2022-12-22 21:24:42 -0300

    codigo-fonte da aula

--- 2.9. O que é injeção de dependências/exemplo-di/src/com/algaworks/di/nitificacao/Notificador.java
+++
@@ -1,8 +0,0 @@
-package com.algaworks.di.nitificacao;
-
-import com.algaworks.di.modelo.Cliente;
-
-public interface Notificador {
-	
-	void notificar(Cliente cliente, String mensagem);
-}
--- 2.9. O que é injeção de dependências/pokemon-injecao-dependencia/src/com/algaworks/di/capturar/Capturar.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.di.capturar;
-
-import com.algaworks.di.modelo.Treinador;
-
-public interface Capturar {
-	
-	public void capturar(Treinador treinador, String mensagem);
-
-}
