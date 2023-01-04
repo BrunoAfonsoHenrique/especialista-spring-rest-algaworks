5ed8f3955ba2d4d958245381d5dca54d2bc471f1
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-04 20:45:38 -0300

    codigo-fonte da aula

--- 2.09. O que é injeção de dependencias/exemplo-injecao-dependencia/src/Main.java
+++ 2.09. O que é injeção de dependencias/exemplo-injecao-dependencia/src/Main.java
@@ -1,8 +1,12 @@
 import com.algaworks.di.modelo.Cliente;
+import com.algaworks.di.modelo.Produto;
 import com.algaworks.di.notificacao.Notificador;
 import com.algaworks.di.notificacao.NotificadorEmail;
 import com.algaworks.di.notificacao.NotificadorSms;
 import com.algaworks.di.service.AtivacaoClienteService;
+import com.algaworks.di.service.EmissaoNotaFiscalService;
+
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +19,21 @@ public class Main {
         AtivacaoClienteService ativacaoCliente =  new AtivacaoClienteService(notificador);
         ativacaoCliente.ativar(joao);
         ativacaoCliente.ativar(maria);
+
+        Notificador notificadorDois = new NotificadorEmail();
+
+        EmissaoNotaFiscalService emissao = new EmissaoNotaFiscalService(notificadorDois);
+        emissao.emitir(joao, new Produto("Panela", BigDecimal.valueOf(50.0)));
+
+        /**
+         * Passo a passo para injetar dependencias e inverter o controle
+         *
+         * Criar um interface com um método abtsrato
+         * Fazer as classes filhas herdarem da interface
+         * Criar uma injecao no service Ex: private Notificador notificador
+         * Criar um construtor
+         * Criar uma instancia na minha classe main. (Ex: Notificador not = new  NotificadorEmail())
+         * e dar um new mais especifico passando parametro
+         */
     }
 }
--- 2.09. O que é injeção de dependencias/exemplo-injecao-dependencia/src/com/algaworks/di/service/EmissaoNotaFiscalService.java
+++ 2.09. O que é injeção de dependencias/exemplo-injecao-dependencia/src/com/algaworks/di/service/EmissaoNotaFiscalService.java
@@ -2,14 +2,21 @@ package com.algaworks.di.service;
 
 import com.algaworks.di.modelo.Cliente;
 import com.algaworks.di.modelo.Produto;
+import com.algaworks.di.notificacao.Notificador;
 import com.algaworks.di.notificacao.NotificadorEmail;
+import com.algaworks.di.notificacao.NotificadorSms;
 
 public class EmissaoNotaFiscalService {
+
+    private Notificador notificador;
+    public EmissaoNotaFiscalService(Notificador notificador) {
+        this.notificador = notificador;
+    }
 
     public void emitir(Cliente cliente, Produto produto) {
         // TODO: Emite a nota fiscal aqui...
 
-        NotificadorEmail notificador = new NotificadorEmail();
+       // NotificadorSms notificador = new NotificadorSms();
         notificador.notificar(cliente, "nota fiscal do produto " +
                 produto.getNome() + " foi emitida!");
 
